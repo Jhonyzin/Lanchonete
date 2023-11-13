b08eba892998b01717131fc021ec32e4402c9b70
FelipeTupi53 <150727386+FelipeTupi53@users.noreply.github.com>
2023-11-13 18:42:59 -0300

    bom dia grupo

--- lanchonete.c
+++ lanchonete.c
@@ -3,13 +3,42 @@
 #include <locale.h>
 #include <string.h>
 #include <unistd.h>
-
+#include "var.h"
 
 
 int main(){
-    printf("TESTANDO!!!!!!!! \n");
-    printf("\n\nMIM MATA PFV!!!!!!!!!!!!");
-    printf("\n\n pq?");
 
+    setlocale(LC_ALL, "Portuguese");
+
+    while(sim!=1){
+        printf("Seja bem-vindo(a) a Lanchonete \n");
+        printf("Deseja ver nosso cardápio? \n");
+        scanf("%s", con);
+
+        if (strcmp(con, "sim") == 0||strcmp(con, "SIM") == 0||strcmp(con, "sIM") == 0||strcmp(con, "SiM") == 0||strcmp(con, "SIm") == 0||strcmp(con, "Sim") == 0){
+            system ("cls");
+            sim++;
+        }
+        else if (strcmp(con, "nao") == 0||strcmp(con, "NAO") == 0||strcmp(con, "nAO") == 0||strcmp(con, "NaO") == 0||strcmp(con, "naO") == 0||strcmp(con, "Nao") == 0){
+            printf ("\nOkay, obrigado!");
+            exit(1);
+        }
+        else{
+            printf("\n\n!!!!!RESPOSTA INVÁLIDA!!!!!\n\n");
+            system("pause");
+            system ("cls");
+        }
+    }
+
+
+    while(1){
+        for(i=0;i<12;i++){
+            if(i==0){printf("\n\nBEBIDAS:\n");} if(i==6){printf("\nCOMIDAS:\n");}
+            printf("%d. %s R$%.2f\n", i,carda[i], valores[i]);
+        }
+        system("pause");
+
+
+    }
     return 0;
 }
---
+++ var.h
@@ -0,0 +1,7 @@
+extern char con[1] = "";
+
+extern int i=0, sim=0;
+
+extern float valores[12]={2,3.5,3.5,5.5,5.5,5.5,4.5,6.9,5.5,6.5,18.9,22};
+
+extern char carda[12][300]={"Agua", "Suco de uva", "Suco de laranja", "Coca-Cola", "Pepsi", "Guarana", "Fanta", "Empada", "Coxinha", "Salada de Frutas", "Hamburguer", "Pizza"};
