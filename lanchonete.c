1d03cdbcd071f2ce240f0f7bee936b84cf13b626
João <143824176+Jhonyzin@users.noreply.github.com>
2023-11-13 22:03:56 -0300

    Atualização 2.0 - Cores

--- lanchonete.c
+++ lanchonete.c
@@ -4,21 +4,53 @@
 #include <string.h>
 #include <unistd.h>
 #include "var.h"
+#include <windows.h>
 
+
+void textColor(int letras, int fundo); // começar
 
 int main(){
+
     setlocale(LC_ALL, "Portuguese");
 
-    printf("Seja bem-vindo(a) ao Lanchonete \n");
 
-    while(1){
+    while(sim!=1){
+        printf("Seja bem-vindo(a) a Lanchonete \n");
         printf("Deseja ver nosso cardápio? \n");
         scanf("%s", con);
 
-    
+        if (strcmp(con, "sim") == 0 || strcmp(con, "SIM") == 0 || strcmp(con, "sIM") == 0 || strcmp(con, "SiM") == 0 || strcmp(con, "SIm") == 0 || strcmp(con, "Sim") == 0 || strcmp(con, "S") == 0 || strcmp(con, "s") == 0){
+            system ("cls");
+            sim++;
+        }
+        else if (strcmp(con, "nao") == 0 || strcmp(con, "NAO") == 0 || strcmp(con, "nAO") == 0 || strcmp(con, "NaO") == 0 || strcmp(con, "naO") == 0 || strcmp(con, "Nao") == 0 || strcmp(con, "N") == 0 || strcmp(con, "n") == 0){
+            printf ("\nOkay, obrigado!");
+            exit(1);
+        }
+        else{
+            textColor(RED, BLACK);
+            printf("\n\n!!!!!RESPOSTA INVÁLIDA!!!!!\n\n");
+            textColor(WHITE, BLACK);
+            system("pause");
+            system ("cls");
+        }
+    }
+
+
+    while(1){
+        for(i=0;i<12;i++){
+            if(i==0){printf("\n\nBEBIDAS:\n");} if(i==6){printf("\nCOMIDAS:\n");}
+            printf("%d. %s R$%.2f\n", i,carda[i], valores[i]);
+        }
+        system("pause");
 
 
     }
-    
     return 0;
 }
+
+
+
+void textColor(int letras, int fundo){
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), letras + fundo);
+} // finaiizar
--- var.h
+++ var.h
@@ -1,1 +1,53 @@
 extern char con[1] = "";
+
+extern int i=0, sim=0;
+
+extern float valores[12]={2,3.5,3.5,5.5,5.5,5.5,4.5,6.9,5.5,6.5,18.9,22};
+
+extern char carda[12][300]={"Agua", "Suco de uva", "Suco de laranja", "Coca-Cola", "Pepsi", "Guarana", "Fanta", "Empada", "Coxinha", "Salada de Frutas", "Hamburguer", "Pizza"};
+
+
+
+//      CORES DAS LETRAS: (1 a 15)
+enum{
+    BLACK,
+    BLUE,
+    GREEN,
+    CYAN,
+    RED,
+    MEGENTA, 
+    BROWN,
+    LIGHTGRAY,
+    DARKGRAY,
+    LIGHTBLUE,
+    LIGHTGREEN,
+    LIGHTCYAN,
+    LIGHTRED,
+    LIGHTMAGENTA,
+    YELLOW, 
+    WHITE
+};
+
+//      CORES DE FUNDO:
+enum{
+    _BLACK = 0,
+    _BLUE = 16,
+    _GREEN = 32,
+    _CYAN = 48,
+    _RED = 64,
+    _MEGENTA = 80, 
+    _BROWN = 96,
+    _LIGHTGRAY = 112,
+    _DARKGRAY = 128,
+    _LIGHTBLUE = 144,
+    _LIGHTGREEN = 160,
+    _LIGHTCYAN = 176,
+    _LIGHTRED = 192,
+    _LIGHTMAGENTA = 208,
+    _YELLOW = 224, 
+    _WHITE = 240 
+};
+
+// EXEMPLO DE COMO USAR:
+// SEMPRE UTILIZE O textColor(a cor da letra, a cor do fundo); (cores de fundo utilizar sem o "_" - underline)
+// sempre começará com a cor que você vai utilizar e vai terminar na cor normal que você precisa mudar também.
